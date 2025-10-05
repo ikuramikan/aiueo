eec9e5b1caee54c3e5bba6f3fbb3327b998db9f1
unknown <ikuramikan@users.noreply.github.com>
2025-10-05 22:03:52 +0900

    update mitamono

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('今日も一日頑張りましょう');
+});
+
--- script.js
+++
@@ -1,4 +0,0 @@
-// ページ読み込み時に簡単なメッセージをコンソールに表示
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('今日も一日頑張りましょう！');
-  });
